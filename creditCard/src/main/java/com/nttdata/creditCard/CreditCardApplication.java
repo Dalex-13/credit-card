6a8bc9f63b4fc93b4931594a00891a79278dd8db
david <davidsza13@gmail.com>
2022-06-04 10:54:19 -0500

    ingreso

---
+++ creditCard/src/main/java/com/nttdata/creditCard/CreditCardApplication.java
@@ -0,0 +1,16 @@
+package com.nttdata.creditCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@EnableEurekaClient
+@SpringBootApplication
+public class CreditCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardApplication.class, args);
+	}
+
+}
